2e2df592d7d98385fee1cbab35a3cf93bd9614ed
Mohan <mohan.ravi@syncfusion.com>
2019-12-28 16:35:19 +0530

    comment quasar

--- client/plugins/quasar.js
+++ client/plugins/quasar.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 
-import Quasar, { QBtn } from 'quasar';
-Vue.use(Quasar, {
-  components: [QBtn],
-});
+// import Quasar, { QBtn } from 'quasar';
+// Vue.use(Quasar, {
+//   components: [QBtn],
+// });
